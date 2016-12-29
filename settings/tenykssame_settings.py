ea09b45546227e26dc6c068191a6689bbc4e2ae0
Quinlan Pfiffer <qpfiffer@gmail.com>
2016-12-29 11:20:00 -0800

    Add Samebow to compose file

---
+++ settings/tenykssame_settings.py
@@ -0,0 +1,10 @@
+DEBUG = True
+SERVICE_NAME = 'TenyksSame'
+SERVICE_VERSION = '0.1.0'
+SERVICE_UUID = '2431a309-6464-435a-b45d-578591681354'
+SERVICE_DESCRIPTION = 'SAME same same same same'
+ZMQ_CONNECTION = {
+    'out': 'tcp://tenyks:61124',
+    'in': 'tcp://tenyks:61123'
+}
+
